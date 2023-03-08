cdc6d4d79f2d345e59b52d1c867e76bb273694b1
TomasCarvalho <2606tom@gmail.com>
2023-03-08 18:06:50 +0000

    Create ManipulaVar; Add suffix incrementation

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(args[0]);
+            x++;
+
+            Console.WriteLine(x);
+        }
+    }
+}
