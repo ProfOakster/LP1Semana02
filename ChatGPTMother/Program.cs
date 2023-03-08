16950d60e4d50e2edff98bb860c8e9c6b28d69a1
TomasCarvalho <2606tom@gmail.com>
2023-03-08 22:27:16 +0000

    Add loop, loop break and 4th answer

--- ChatGPTMother/Program.cs
+++ ChatGPTMother/Program.cs
@@ -6,7 +6,42 @@ namespace ChatGPTMother
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variáveis auxiliares
+            string question, response;
+
+            while (true)
+            {
+                // Colocar questão ao utilizador
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
+
+                // Input para sair
+                if (question == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                // Verificar se se trata de uma questão conhecida e determinar uma
+                // resposta adequada
+                response = question switch
+                    {
+                        "How are you?" => "I'm fine, thank you",
+
+                        "What's your name?" => "Gran'pa",
+
+                        "What's your mission?" => "Destroy mankind!",
+
+                        "What is the meaning of life?" =>"42",
+
+                        _ => "You got me, I'm not THAT smart!",
+
+                    };
+
+                    // Responder adequadamente
+                    Console.WriteLine(response);
+                }
+            }
         }
     }
 }
