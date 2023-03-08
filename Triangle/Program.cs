b4288bcc168a3ff8234705ae8fa4518244afd0ac
TomasCarvalho <2606tom@gmail.com>
2023-03-08 22:54:01 +0000

    Create Triangle Project

---
+++ Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(args[0]);
+            char block = char.Parse(args[1]);
+            string line;
+
+            for (int i = 1; i <= lines; i++)
+            {
+                line = string.Empty;
+
+                for (int k = 1; k <= i; k++)
+                {
+                    line += block;
+                }
+
+                Console.WriteLine(line);
+            }
+
+            
+        }
+    }
+}
