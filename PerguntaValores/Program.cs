bfc7a7e58c6f9fbba15bf12e137b86646fa7f411
TomasCarvalho <2606tom@gmail.com>
2023-03-08 19:05:46 +0000

    Change calculations to use Math.__ functions

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -16,13 +16,8 @@ namespace PerguntaValores
             inp = Console.ReadLine();
             r = float.Parse(inp);
 
-<<<<<<< HEAD
             float v = Math.PI * Math.Pow(r,2) * a;
             float sa = 2 * Math.PI * r * (r + a);
-=======
-            float v = 3.1415926f * r * r * a;
-            float sa = 2 * 3.1415926f * r * (r + a);
->>>>>>> fd0c272946e10dc0d6eff4767bc0616f374aee05
 
             Console.WriteLine(
                 $"Cylinder volume is {v}.\nCylinder surface area is {sa}."
