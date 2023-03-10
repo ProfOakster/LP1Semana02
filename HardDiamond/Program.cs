2f314edce198b0190efeed55d21310bbb6d47318
TomasCarvalho <2606tom@gmail.com>
2023-03-10 10:59:44 +0000

    Remove unnecessary code

--- HardDiamond/Program.cs
+++ HardDiamond/Program.cs
@@ -19,13 +19,12 @@ namespace HardDiamond
                 //Cycles through i from (-size/2) to (size/2) 
                 for (int i = -1*size/2; i <= size/2; i++)
                 {
-                    //The # of spaces to write before and after 
-                    //the dots is |i|
+                    //The # of spaces to write is |i|
                     string space = new string(' ', Math.Abs(i));
                     //The # of dots to write is size-|i|
                     string dot = new string('.', size-(Math.Abs(i)*2));
                     //Writes line to console
-                    Console.WriteLine(space+dot+space);
+                    Console.WriteLine(space+dot);
                 }
             }
         }
