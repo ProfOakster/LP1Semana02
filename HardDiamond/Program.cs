58f9e8f24ecc7e13c1eedcdc964c2c2cf0690cf9
TomasCarvalho <2606tom@gmail.com>
2023-03-10 10:54:43 +0000

    Add code comments

--- HardDiamond/Program.cs
+++ HardDiamond/Program.cs
@@ -16,23 +16,18 @@ namespace HardDiamond
             }
             else
             {
+                //Cycles through i from (-size/2) to (size/2) 
                 for (int i = -1*size/2; i <= size/2; i++)
                 {
+                    //The # of spaces to write before and after 
+                    //the dots is |i|
                     string space = new string(' ', Math.Abs(i));
-                    string dot = new string('.', Math.Abs(size)-(Math.Abs(i)*2));
+                    //The # of dots to write is size-|i|
+                    string dot = new string('.', size-(Math.Abs(i)*2));
+                    //Writes line to console
                     Console.WriteLine(space+dot+space);
                 }
-
-
-
-
-
             }
-
-
-
-
-            
         }
     }
 }
